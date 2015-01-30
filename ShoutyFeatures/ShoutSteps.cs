599e08c1afd5d93ee0add69cafad474815a6c09d
Steve Tooke <steve@boxjump.co.uk>
2015-01-30 11:40:12 +0000

    Generate pending step definitions

--- ShoutyFeatures/ShoutSteps.cs
+++ ShoutyFeatures/ShoutSteps.cs
@@ -8,6 +8,22 @@ namespace ShoutyFeatures
   [Binding]
   public class ShoutSteps
   {
+    [Given(@"Lucy is (.*) metres from Sean")]
+    public void GivenLucyIsMetresFromSean(int p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
 
+    [When(@"Sean shouts ""(.*)""")]
+    public void WhenSeanShouts(string p0)
+    {
+      ScenarioContext.Current.Pending();
+    }
+
+    [Then(@"Lucy hears Sean's message")]
+    public void ThenLucyHearsSeanSMessage()
+    {
+      ScenarioContext.Current.Pending();
+    }
   }
 }
