a1a7417661fdad0a84cd690bd8feafd80b351a0d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-03-04 14:07:16 +0000

    Blank slate for WebDriver Stepdefs

--- ShoutyFeatures/ShoutWebSteps.cs
+++ ShoutyFeatures/ShoutWebSteps.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 
 namespace ShoutyFeatures
@@ -6,17 +8,61 @@ namespace ShoutyFeatures
     [Binding, Scope(Tag = "web")]
     public class ShoutWebSteps
     {
+        private IWebDriver _browser;
+
         public ShoutWebSteps(BrowserContext browserContext)
         {
-            var browser = browserContext.Browser;
-            browser.Navigate().GoToUrl(WebHooks.Url + "/people/John");
-            Thread.Sleep(5000);
+            _browser = browserContext.Browser;
+            //_browser.Navigate().GoToUrl(WebHooks.Url + "/people/John");
         }
 
         [Given(@"the following locations:")]
         public void GivenTheFollowingLocations(Table table)
         {
-            // Copy from other stepdef? Factor it out to other class?
+            throw new Exception("FIXME");
+        }
+
+        [Given(@"(.*) is around")]
+        public void GivenPersonIsAround(string personName)
+        {
+            throw new Exception("FIXME");
+        }
+
+        [Given(@"(.*) is in (.*)")]
+        public void GivenPersonIsInLocation(string personName, string locationName)
+        {
+            throw new Exception("FIXME");
+        }
+
+        [When(@"(.*) shouts")]
+        public void WhenPersonShouts(string personName)
+        {
+            throw new Exception("FIXME");
+        }
+
+        [When(@"(.*) shouts ""(.*)""")]
+        public void WhenPersonShouts(string personName, string message)
+        {
+            throw new Exception("FIXME");
+        }
+
+        [Then(@"(.*) should hear:")]
+        public void ThenPersonShouldHear(string personName, Table expectedShoutsTable)
+        {
+            throw new Exception("FIXME");
+        }
+
+
+        [Then(@"(.*) should not hear anything")]
+        public void ThenPersonShouldNotHearAnything(string personName)
+        {
+            throw new Exception("FIXME");
+        }
+
+        [Then(@"(.*) should hear ""(.*)""")]
+        public void ThenPerspnShouldHearMessage(string personName, string expectedMessage)
+        {
+            throw new Exception("FIXME");
         }
     }
 }
