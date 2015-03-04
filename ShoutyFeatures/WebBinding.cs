d135fdd384a697724fa76926aa030b0f68a8e86a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-03-04 08:53:46 +0000

    Start and stop server and browser

--- ShoutyFeatures/WebBinding.cs
+++ ShoutyFeatures/WebBinding.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.Hosting.Self;
+using Nancy.TinyIoc;
+using Nancy.ViewEngines;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using TechTalk.SpecFlow;
@@ -32,7 +39,7 @@ namespace ShoutyFeatures
         public void OpenBrowser()
         {
             _browser = new InternetExplorerDriver();
-            _browser.Navigate().GoToUrl(Url);
+            _browser.Navigate().GoToUrl(Url + "/people/John");
         }
 
         [AfterScenario()]
@@ -46,6 +53,19 @@ namespace ShoutyFeatures
         {
             _server.Stop();
             _server.Dispose();
+        }
+    }
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer c, IPipelines p)
+        {
+            this.Conventions.ViewLocationConventions.Add((viewName, model, context) =>
+            {
+                // This doesn't seem to work :-(
+                // So we copy the views for now...
+                return string.Concat("../../../ShoutyWeb/views/", viewName);
+            });   
         }
     }
 }
--- ShoutyWeb/ShoutyModule.cs
+++ ShoutyWeb/ShoutyModule.cs
@@ -1,12 +1,40 @@
-﻿using Nancy;
+﻿using System;
+using System.Collections.Generic;
+using Nancy;
+using Shouty;
 
 namespace ShoutyWeb
 {
     public class ShoutyModule : NancyModule
     {
+        private static readonly ShoutyApi _shoutyApi = new ShoutyApi();
+
         public ShoutyModule()
         {
-            Get["/"] = parameters => "Hello World!!!";
+            Console.WriteLine("ShoutyModule created");
+
+            Get["/"] = _ => View["index.html"];
+
+            Get["/people/{personName}"] = _ =>
+            {
+                Dictionary<string, object> model = new Dictionary<string, object>()
+                {
+                    {"personName", _.personName},
+                    {"messages", _shoutyApi.MessagesHeardBy(_.personName)}
+                };
+                return View["person.html", model];
+            };
+
+            Post["/messages"] = _ =>
+            {
+                string personName = Request.Form["personName"];
+                string message = Request.Form["message"];
+                Console.WriteLine(personName + ":" + message);
+                _shoutyApi.PersonShouts(personName, message);
+                var heard = _shoutyApi.MessagesHeardBy(personName);
+                Console.WriteLine(heard);
+                return Response.AsRedirect(Request.Headers.Referrer);
+            };
         }
     }
 }
