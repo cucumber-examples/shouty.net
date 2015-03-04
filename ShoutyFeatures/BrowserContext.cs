6d3bf03ed6aa179db52b140edac2668792a21c68
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-03-04 12:15:03 +0000

    Add ChromeDriver

--- ShoutyFeatures/BrowserContext.cs
+++ ShoutyFeatures/BrowserContext.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 
 namespace ShoutyFeatures
 {
     public class BrowserContext
     {
-        private IWebDriver _browser = new InternetExplorerDriver("../..");
+        //private readonly IWebDriver _browser = new ChromeDriver("../..");
+        private readonly IWebDriver _browser = new InternetExplorerDriver("../..");
 
         public IWebDriver Browser
         {
