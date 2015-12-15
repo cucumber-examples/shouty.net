f5ac7b18d93a49019cd70b05eabbe4f5f6ae365a
Chris Parsons <chris@thinkcodelearn.com>
2015-12-15 14:19:12 +0100

    One passing, one failing scenario

--- ShoutyFeatures/HearShout.feature
+++ ShoutyFeatures/HearShout.feature
@@ -1,5 +1,21 @@
-﻿Feature: Hear shout
+﻿Feature: User hears shout
 
-  A Description goes here
-	
-  Scenario: What should it do?
+	In order to hear some cool and useful info
+	Lucy should be able to hear Bob shouting
+	about free stuff in her area
+
+	Rules
+	=====
+
+	- People must be 1000m or closer to hear each other
+	- Only consider shouts as happening immediately
+
+  Scenario: Lucy doesn't hear Bob as she is 1100m away
+	Given Lucy and Bob are 1100m apart
+	When Bobs shouts "Free stuff!"
+	Then Lucy doesn't hear anything
+
+  Scenario: Lucy hears Bob as they are only 400m apart
+	Given Lucy and Bob are 400m apart
+	When Bobs shouts "Free stuff!"
+	Then Lucy hears "Free stuff!"
---
+++ ShoutyFeatures/UserHearsShoutSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+class ShoutyMainApi
+{
+    public void SetLocation(string name, int location)
+    {
+
+    }
+
+    public void Shout(string name, string message)
+    {
+
+    }
+
+    public List<string> messagesHeard(string name)
+    {
+        return new List<string>();
+    }
+};
+
+namespace ShoutyFeatures
+{
+    [Binding]
+    public class UserHearsShoutSteps
+    {
+        private ShoutyMainApi shouty = new ShoutyMainApi();
+
+        [Given(@"Lucy and Bob are (.*)m apart")]
+        public void GivenLucyAndBobAreMApart(int distance)
+        {
+            shouty.SetLocation("Lucy", 0);
+            shouty.SetLocation("Bob", distance);
+        }
+
+        [When(@"Bobs shouts ""(.*)""")]
+        public void WhenBobsShouts(string message)
+        {
+            shouty.Shout("Bob", message);
+        }
+
+        [Then(@"Lucy hears ""(.*)""")]
+        public void ThenLucyHears(string message)
+        {
+            Assert.That(shouty.messagesHeard("Lucy"), Contains.Item(message));
+        }
+
+        [Then(@"Lucy doesn't hear anything")]
+        public void ThenLucyDoesnTHearAnything()
+        {
+            Assert.That(shouty.messagesHeard("Lucy"), Is.Empty);
+            // Assert.Equals(shouty.messagesHeard("Lucy").count, 0);
+        }
+    }
+}
