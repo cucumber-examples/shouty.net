ce9e25ec969686ff5742c80b4f33fa9481303f63
Chris Parsons <chris@thinkcodelearn.com>
2015-12-15 15:26:47 +0100

    Both tests passing

--- Shouty/Shouty.cs
+++ Shouty/Shouty.cs
@@ -6,21 +6,55 @@ using System.Threading.Tasks;
 
 namespace Shouty
 {
+    public class Person
+    {
+        public int location = 0;
+        public List<string> messagesHeard = new List<string>();
+
+        public void Hear(string message)
+        {
+            messagesHeard.Add(message);
+        }
+
+        public bool withinRangeOf(Person other)
+        {
+            return Math.Abs(other.location - this.location) <= 1000;
+        }
+    };
+
     public class ShoutyMainApi
     {
+        private Person findOrCreatePerson(string name)
+        {
+            Person value = new Person();
+            if (!people.TryGetValue(name, out value))
+            {
+                people.Add(name, new Person());
+            }
+            return people[name];
+        }
+
+        private Dictionary<String, Person> people = new Dictionary<string, Person>();
         public void SetLocation(string name, int location)
         {
-
+            findOrCreatePerson(name).location = location;
         }
 
         public void Shout(string name, string message)
         {
-
+            Person shouter = findOrCreatePerson(name);
+            foreach (Person potentialHearer in people.Values)
+            {
+                if (potentialHearer.withinRangeOf(shouter))
+                {
+                    potentialHearer.Hear(message);
+                }
+            }
         }
 
         public List<string> messagesHeard(string name)
         {
-            return new List<string>();
+            return findOrCreatePerson(name).messagesHeard;
         }
     };
 }
--- ShoutyTests/UnitTest1.cs
+++ ShoutyTests/UnitTest1.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using NUnit.Framework;
+using Shouty;
 
 namespace ShoutyTests
 {
     [TestFixture]
-    public class UnitTest1
+    public class PersonTest
     {
         [Test]
-        public void TestMethod1()
+        public void HasntHeardAnyMessagesAtStart()
         {
+           Assert.That(new Person().messagesHeard, Is.Empty);
+        }
+
+        [Test]
+        public void HeardMessage()
+        {
+            Person person = new Person();
+            person.Hear("Hello");
+            Assert.That(person.messagesHeard, Contains.Item("Hello"));
+        }
+
+        [Test]
+        public void WithinRangeIsTrueIf1000OrLess()
+        {
+            Person person = new Person();
+            Person other = new Person();
+            other.location = 1000;
+            Assert.True(person.withinRangeOf(other));
+        }
+
+        [Test]
+        public void OutsideRangeIfGreaterThan1000()
+        {
+            Person person = new Person();
+            Person other = new Person();
+            other.location = 1001;
+            Assert.False(person.withinRangeOf(other));
         }
     }
 }
