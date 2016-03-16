10607ff6adcaf6e14aa9aa0c9ae52009c5f2886c
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-03-16 10:34:39 +0100

    add unit test project

---
+++ Shouty.UnitTests/LocationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Shouty.UnitTests
+{
+    [TestFixture]
+    public class LocationTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
