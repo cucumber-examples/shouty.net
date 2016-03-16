9a16c6128f5b7ad5de229e2e6c48cdf5932a5c23
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-03-16 11:54:28 +0100

    automate heard scenario

--- ShoutyFeatures/StepDefinitions/WebSteps.cs
+++ ShoutyFeatures/StepDefinitions/WebSteps.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using Shouty;
 using TechTalk.SpecFlow;
 
@@ -12,28 +14,62 @@ namespace ShoutyFeatures.StepDefinitions
     [Binding, Scope(Tag = "web")]
     public class WebSteps
     {
+        private readonly IWebDriver webDriver = new FirefoxDriver();
+        private const string baseUrl = "http://localhost:13636";
+
         [Given(@"(.*) is at (.*)")]
         public void GivenPersonIsAt(string name, Location location)
         {
-            ScenarioContext.Current.Pending();
+            GoToPersonPage(name);
+            var latTextBox = webDriver.FindElement(By.Id("lat"));
+            latTextBox.SendKeys(location.Lat.ToString());
+
+            var lonTextBox = webDriver.FindElement(By.Id("lon"));
+            lonTextBox.SendKeys(location.Lon.ToString());
+
+            lonTextBox.Submit();
+        }
+
+        private void GoToPersonPage(string name)
+        {
+            webDriver.Navigate().GoToUrl(baseUrl + "/Person/Index/" + name);
         }
 
         [When(@"Fred shouts")]
         public void WhenFredShouts()
         {
-            ScenarioContext.Current.Pending();
+            GoToPersonPage("Fred");
+
+            var messageTextBox = webDriver.FindElement(By.Id("message"));
+            messageTextBox.SendKeys("hello world!");
+
+            messageTextBox.Submit();
         }
 
         [Then(@"Linda should hear nothing")]
         public void ThenLindaShouldHearNothing()
         {
-            ScenarioContext.Current.Pending();
+            var messages = GetHeardMessages("Linda");
+            Assert.AreEqual(0, messages.Count, "Should hear nothing");
         }
 
         [Then(@"Linda should hear Fred’s shout")]
         public void ThenLindaShouldHearFredSShout()
         {
             ScenarioContext.Current.Pending();
+        }
+
+        private List<string> GetHeardMessages(string name)
+        {
+            GoToPersonPage(name);
+            var messages = webDriver.FindElements(By.ClassName("message")).Select(e => e.Text).ToList();
+            return messages;
+        }
+
+        [AfterScenario]
+        public void CloseBrowser()
+        {
+            webDriver.Quit();
         }
     }
 }
