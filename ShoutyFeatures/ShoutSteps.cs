d2587e672d020b0969ead2e3ad7b3a9e3bd6050f
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-03-16 09:53:07 +0100

    Add step definition snippets

--- ShoutyFeatures/ShoutSteps.cs
+++ ShoutyFeatures/ShoutSteps.cs
@@ -5,9 +5,25 @@ using Shouty;
 
 namespace ShoutyFeatures
 {
-  [Binding]
-  public class ShoutSteps
-  {
+    [Binding]
+    public class ShoutSteps
+    {
+        [Given(@"Linda is (.*)m away from Fred")]
+        public void GivenLindaIsMAwayFromFred(int distanceInMetres)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
-  }
+        [When(@"Fred shouts")]
+        public void WhenFredShouts()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Linda should hear nothing")]
+        public void ThenLindaShouldHearNothing()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
 }
