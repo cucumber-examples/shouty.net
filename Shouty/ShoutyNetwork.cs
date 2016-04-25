fce6f738a97f7398a61d998a41e1934668e069bf
Steve Tooke <steve@boxjump.co.uk>
2016-04-25 22:07:41 +0100

    Bring start point up to date with Java

---
+++ Shouty/ShoutyNetwork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shouty
+{
+  public class ShoutyNetwork
+  {
+    private Dictionary<string, Coordinate> locationsByPerson = new Dictionary<string, Coordinate>();
+    private Dictionary<string, string> messagesByPerson = new Dictionary<string, string>();
+    private const int MESSAGE_RANGE = 1000;
+    
+    public void SetLocation(string personName, Coordinate coordinate)
+    {
+      locationsByPerson.Add(personName, coordinate);
+    }
+
+    public void Shout(string shouterName, string message)
+    {
+      messagesByPerson.Add(shouterName, message);
+    }
+
+    public IDictionary<string, string> GetMessagesHeardBy(string listenerName)
+    {
+      var result = new Dictionary<string, string>();
+
+      foreach(var shout in messagesByPerson)
+      {
+        var shouter = shout.Key;
+        var message = shout.Value;
+
+        int distance = locationsByPerson[shouter].
+          DistanceFrom(locationsByPerson[listenerName]);
+
+        if (distance < MESSAGE_RANGE)
+          result.Add(shouter, message);
+      }
+
+      return result;
+    }
+  }
+}
