2c3c74e8609428ceba487d0f2fcfc4fd7baeac4a
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-05-20 14:32:00 +0200

    P19-hooks: Add before/after hooks

---
+++ Shouty.Specs/ShoutyHooks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Shouty.Specs
+{
+    [Binding]
+    public class ShoutyHooks
+    {
+        [BeforeScenario]
+        public void PrintBeforeMessage()
+        {
+            Console.WriteLine("this is before");
+        }
+
+        [AfterScenario]
+        public void PrintAfterMessage()
+        {
+            Console.WriteLine("this is after");
+        }
+    }
+}
