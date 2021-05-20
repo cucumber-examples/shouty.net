153ccf26bbbae3e9572b4c25fb6b544f97bf4a50
Gaspar Nagy <gaspar.nagy@gmail.com>
2021-05-20 14:36:28 +0200

    P19-hooks: Add ordered hooks

--- Shouty.Specs/ShoutyHooks.cs
+++ Shouty.Specs/ShoutyHooks.cs
@@ -6,16 +6,28 @@ namespace Shouty.Specs
     [Binding]
     public class ShoutyHooks
     {
-        [BeforeScenario]
+        [BeforeScenario(Order = 1)]
         public void PrintBeforeMessage()
         {
             Console.WriteLine("this is before");
         }
 
-        [AfterScenario]
+        [AfterScenario(Order = 1)]
         public void PrintAfterMessage()
         {
             Console.WriteLine("this is after");
+        }
+
+        [BeforeScenario(Order = 2)]
+        public void OtherPrintBeforeMessage()
+        {
+            Console.WriteLine("this is other before");
+        }
+
+        [AfterScenario(Order = 2)]
+        public void OtherPrintAfterMessage()
+        {
+            Console.WriteLine("this is other after");
         }
     }
 }
